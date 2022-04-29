9eb5b6dc8976391eea678db8ff2e08f98d6ff047
Moacir Junior <skytec.dev@gmail.com>
2022-04-29 00:05:16 -0300

    Alterado o texto do botão

--- lib/home.dart
+++ lib/home.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
                     ),
                     backgroundColor: MaterialStateProperty.all(Colors.amber),
                     foregroundColor: MaterialStateProperty.all(Colors.white)),
-                child: const Text('Atualizar'),
+                child: const Text('Atualizar Preço'),
               ),
             ),
           ],
